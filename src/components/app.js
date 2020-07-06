38907c45775e45fcfa8a3253d855d1d9dbbcf8e0
BoddieMeaghan <Boddie.Meaghan@gmail.com>
2020-07-05 22:38:24 -0500

    Added info on JSON

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import moment from 'moment'; 
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>F.A.M. Project</h1>
+        <h2>Feeding Another Mind</h2>
       </div>
     );
   }
