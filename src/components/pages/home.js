5b78f5b3e9e9ca373403a502b6689cbafe0c3785
BoddieMeaghan <Boddie.Meaghan@gmail.com>
2020-07-07 23:23:31 -0500

    Added home notes

---
+++ src/components/pages/home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+ 
+
+        export default class App extends Component {
+        render() {
+            return (
+            <div className='app'>
+                <h1>Homescreen</h1>
+                <h2>Feeding Another Mind</h2>
+            </div>
+                );
+            }
+        }
+    }
+}
+//   <div> map goes here </div>
+//   <div> restaurant list goes here </div>
+//   <div> map goes here</div>
+{/* <div> navbar </div> */}
