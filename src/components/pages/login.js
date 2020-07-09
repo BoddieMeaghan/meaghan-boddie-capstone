574e6648ca4962efad59cdc310393199f8f57bd7
BoddieMeaghan <Boddie.Meaghan@gmail.com>
2020-07-09 15:11:50 -0500

    Add map and colors in scss

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,13 +1,3 @@
 import React, { Component } from 'react';
  
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>F.A.M. Project</h1>
-        <h2>Feeding Another Mind</h2>
-      </div>
-    );
-  }
-}
---
+++ src/components/pages/welcome.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function() {
+  return (
+    <div>
+      <WelcomeContainer />
+        <div class="map">
+
+       
+        </div>
+            <div class="listings"></div>
+            <div class="regloginbar"></div>
+    </div>
+  );
+}
+
+
+
